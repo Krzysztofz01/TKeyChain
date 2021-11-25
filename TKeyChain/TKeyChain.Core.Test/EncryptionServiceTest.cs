947dba90940896e3517eebd030aa8f272db3d413
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-11-25 19:20:18 +0100

    Encryption implementation and tests

---
+++ TKeyChain/TKeyChain.Core.Test/EncryptionServiceTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Security.Cryptography;
+using TKeyChain.Core.Abstraction;
+using Xunit;
+
+namespace TKeyChain.Core.Test
+{
+    public class EncryptionServiceTest
+    {
+        [Fact]
+        public void ServiceShouldEncryptValidString()
+        {
+            // Arrange
+            string plainText = "Hello world!";
+            
+            string correctKey = "My key!";
+
+            IEncryptionService encryptionService = new EncryptionService();
+
+            // Operation
+            string cipher = encryptionService.EncryptVault(plainText, correctKey);
+
+            // Assertion
+            Assert.NotNull(cipher);
+        }
+
+        [Fact]
+        public void ServiceShouldDecryptWithValidKey()
+        {
+            // Arrange
+            string plainText = "Hello world!";
+
+            string correctKey = "My key!";
+
+            IEncryptionService encryptionService = new EncryptionService();
+
+            // Operation
+            string cipher = encryptionService.EncryptVault(plainText, correctKey);
+
+            IEncryptionService encryptionServiceForDecryption = new EncryptionService();
+            string decryptedText = encryptionServiceForDecryption.DecryptVault(cipher, correctKey);
+
+            // Assertion
+            Assert.Equal(plainText, decryptedText);
+        }
+
+        [Fact]
+        public void ServiceShouldNotDecryptWithInvalidKey()
+        {
+            // Arrange
+            string plainText = "Hello world!";
+
+            string correctKey = "My key!";
+            string wrongKey = "Not my key!";
+
+            IEncryptionService encryptionService = new EncryptionService();
+
+            // Operation
+            string cipher = encryptionService.EncryptVault(plainText, correctKey);
+
+            IEncryptionService encryptionServiceForDecryption = new EncryptionService();
+
+            // Assertion
+            Assert.Throws<CryptographicException>(() => encryptionServiceForDecryption.DecryptVault(cipher, wrongKey));
+        }
+    }
+}
---
+++ TKeyChain/TKeyChain.Core/EncryptionService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Security.Cryptography;
+using System.Text;
+using TKeyChain.Core.Abstraction;
+
+namespace TKeyChain.Core
+{
+    public class EncryptionService : IEncryptionService
+    {
+        private const int _saltSize = 8;
+        private const int _bytesDeriveIterations = 1000;
+
+        public string DecryptVault(string vaultCipher, string key)
+        {
+            Span<byte> encryptedData = Convert.FromBase64String(vaultCipher).AsSpan();
+
+            using var aesGcm = CreateAesGcm(key);
+
+            int nonceSize = BinaryPrimitives.ReadInt32LittleEndian(encryptedData.Slice(0, 4));
+            int tagSize = BinaryPrimitives.ReadInt32LittleEndian(encryptedData.Slice(4 + nonceSize, 4));
+            int cipherSize = encryptedData.Length - 4 - nonceSize - 4 - tagSize;
+
+            var nonce = encryptedData.Slice(4, nonceSize);
+            var tag = encryptedData.Slice(4 + nonceSize + 4, tagSize);
+            var cipherBytes = encryptedData.Slice(4 + nonceSize + 4 + tagSize, cipherSize);
+
+            Span<byte> plainBytes = cipherSize < 1024
+                ? stackalloc byte[cipherSize]
+                : new byte[cipherSize];
+
+            aesGcm.Decrypt(nonce, cipherBytes, tag, plainBytes);
+
+            return Encoding.UTF8.GetString(plainBytes);
+        }
+
+        public string EncryptVault(string vaultSerialized, string key)
+        {
+            byte[] plainBytes = Encoding.UTF8.GetBytes(vaultSerialized);
+
+            using var aesGcm = CreateAesGcm(key);
+
+            int nonceSize = AesGcm.NonceByteSizes.MaxSize;
+            int tagSize = AesGcm.TagByteSizes.MaxSize;
+            int cipherSize = plainBytes.Length;
+
+            int encryptedDataLength = 4 + nonceSize + 4 + tagSize + cipherSize;
+            
+            Span<byte> encryptedData = encryptedDataLength < 1024
+                ? stackalloc byte[encryptedDataLength]
+                : new byte[encryptedDataLength].AsSpan();
+
+            BinaryPrimitives.WriteInt32LittleEndian(encryptedData.Slice(0, 4), nonceSize);
+            BinaryPrimitives.WriteInt32LittleEndian(encryptedData.Slice(4 + nonceSize, 4), tagSize);
+
+            var nonce = encryptedData.Slice(4, nonceSize);
+            var tag = encryptedData.Slice(4 + nonceSize + 4, tagSize);
+            var cipherBytes = encryptedData.Slice(4 + nonceSize + 4 + tagSize, cipherSize);
+
+            RandomNumberGenerator.Fill(nonce);
+
+            aesGcm.Encrypt(nonce, plainBytes.AsSpan(), cipherBytes, tag);
+
+            return Convert.ToBase64String(encryptedData);
+        }
+
+        private AesGcm CreateAesGcm(string key)
+        {
+            byte[] salt = new byte[_saltSize];
+
+            byte[] keyBytes = new Rfc2898DeriveBytes(key, salt, _bytesDeriveIterations).GetBytes(16);
+
+            return new AesGcm(keyBytes);
+        }
+    }
+}
